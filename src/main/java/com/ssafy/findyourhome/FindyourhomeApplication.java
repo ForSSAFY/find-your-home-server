974f36d76bf92843ecc1c12d7e85b57f1a6e6635
Lechros <0onionion0@gmail.com>
2023-11-13 14:10:21 +0900

    fresh start

---
+++ src/main/java/com/ssafy/findyourhome/FindyourhomeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.findyourhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindyourhomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindyourhomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssafy/findyourhome/FindyourhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.findyourhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindyourhomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
