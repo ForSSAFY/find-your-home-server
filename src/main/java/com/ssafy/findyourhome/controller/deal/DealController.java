264b27422483bb3501aaf0b19d08be344b2ef886
ryuu9599 <ryuu9599@gmail.com>
2023-11-15 13:53:51 +0900

    refactor: deal logics to returns ResponseEntity

--- src/main/java/com/ssafy/findyourhome/controller/deal/DealController.java
+++ src/main/java/com/ssafy/findyourhome/controller/deal/DealController.java
@@ -1,23 +1,19 @@
 package com.ssafy.findyourhome.controller.deal;
 
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ssafy.findyourhome.dto.deal.HouseDealInfoDto;
 import com.ssafy.findyourhome.service.deal.DealService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/deal")
@@ -26,61 +22,61 @@ import lombok.extern.slf4j.Slf4j;
 public class DealController {
 
     private final DealService dealService;
-    private final ObjectMapper objectMapper;
     private final String NAME = "[DealController]";
 
-    @GetMapping
-    public ResponseEntity<String> getDeal() throws SQLException, JsonProcessingException {
-        List<String> sidos = dealService.getSidos();
-        List<Integer> years = dealService.getYears();
-
-        Map<String, Object> responseData = new HashMap<>();
-        responseData.put("sidos", sidos);
-        responseData.put("years", years);
-        String jsonResponse = objectMapper.writeValueAsString(responseData);
-        return ResponseEntity.ok(jsonResponse);
-    }
-
     @GetMapping("/sido")
-    public String getSido() throws SQLException, JsonProcessingException {
+    public ResponseEntity<?> getSido() throws SQLException {
         List<String> result = dealService.getSidos();
-        String jsonResult = objectMapper.writeValueAsString(result);
-        log.info("jsonResult: {}", jsonResult);
-        return jsonResult;
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(result);
     }
 
     @GetMapping("/gugun")
-    public String getGugun(@RequestParam String sido) throws JsonProcessingException, SQLException {
+    public ResponseEntity<?> getGugun(@RequestParam String sido) throws SQLException {
         log.info("{} getGugun by sido({})", NAME, sido);
         List<String> result = dealService.getGuguns(sido);
-        String jsonResult = objectMapper.writeValueAsString(result);
-        log.info("jsonResult: {}", jsonResult);
-        return jsonResult;
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(result);
     }
 
     @GetMapping("/dong")
-    public String getDong(@RequestParam String sido, @RequestParam String gugun) throws JsonProcessingException, SQLException {
+    public ResponseEntity<?> getDong(@RequestParam String sido, @RequestParam String gugun) throws SQLException {
         log.info("{} getDong by sido({}), gugun({})", NAME, sido, gugun);
         List<String> result = dealService.getDongs(sido, gugun);
-        String jsonResult = objectMapper.writeValueAsString(result);
-        log.info("jsonResult: {}", jsonResult);
-        return jsonResult;
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(result);
     }
 
-    @GetMapping("/deal")
-    public String getDeal(@RequestParam String sido, @RequestParam String gugun, @RequestParam String dong, @RequestParam String year, @RequestParam String month) throws SQLException, JsonProcessingException {
-        int dealYear;
-        int dealMonth;
+    /*@GetMapping
+    public ResponseEntity<?> getDeal() throws SQLException {
+        List<String> sidos = dealService.getSidos();
+        List<Integer> years = dealService.getYears();
+        Map<String, Object> responseData = new HashMap<>();
+        responseData.put("sidos", sidos);
+        responseData.put("years", years);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(responseData);
+    }*/
+
+    @GetMapping
+    public ResponseEntity<?> getDeal(@RequestParam String sido, @RequestParam String gugun, @RequestParam String dong, @RequestParam String year, @RequestParam String month) throws SQLException, JsonProcessingException {
+        int dealYear, dealMonth;
         try {
             dealYear = Integer.parseInt(year);
             dealMonth = Integer.parseInt(month);
         } catch (NumberFormatException e) {
             e.printStackTrace();
-            return "[]";
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(null);
         }
         List<HouseDealInfoDto> result = dealService.getHouseDeals(sido, gugun, dong, dealYear, dealMonth);
-        String jsonResult = objectMapper.writeValueAsString(result);
-        log.info("jsonResult: {}", jsonResult);
-        return jsonResult;
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(result);
     }
 }
